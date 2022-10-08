d4781bf94aac662a71b15f3567f75255affc940d
Sasha Sheng <sash@fb.com>
2022-10-08 13:36:13 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function setup(){
+  
+    //Initialisation of canvas, objects, variables etc.
+    
+  }
+  
+  function draw(){
+    
+    //The drawing loop - updates at a constant rate declared in setup
+    
+  }
